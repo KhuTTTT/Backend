a2ef9c53da69b6d1a3740cf81d6471c1ce75ef6d
sunwupark <sunwu5678@gmail.com>
2023-11-11 03:33:02 +0900

    git update: pts.py

--- pts.py
+++ pts.py
@@ -7,7 +7,7 @@ def PdfToString(path):
     number_of_pages = len(reader.pages)
     page = reader.pages[0]
     text = ""
-    for i in range(4):
+    for i in range(number_of_pages):
         page = reader.pages[i]
         text += page.extract_text()
     return text
